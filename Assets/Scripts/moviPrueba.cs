e4cd630c2dbce6dfb2a8c6ce088e154868d21559
PaolaCieza <paolacieza8@gmail.com>
2021-11-17 18:18:04 -0500

    escena 1 grande

--- Assets/Scripts/moviPrueba.cs
+++ Assets/Scripts/moviPrueba.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class movi : MonoBehaviour
+public class moviPrueba : MonoBehaviour
 {
     // Camera mycam;
     // void Start()
