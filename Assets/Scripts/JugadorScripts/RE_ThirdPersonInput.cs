8cb2753d77b3431941657e5beca7cab2732bf2e6
jesuslujan98 <jesusfer_nan2@hotmail.com>
2021-11-19 04:48:44 -0500

    agregando bugs

--- Assets/Scripts/JugadorScripts/RE_ThirdPersonInput.cs
+++ Assets/Scripts/JugadorScripts/RE_ThirdPersonInput.cs
@@ -15,7 +15,8 @@ public class RE_ThirdPersonInput : ThirdPersonUserControl
     public Transform chest;     // El pecho del personaje. Se usa para rotar el personaje hacia la dirección en la que queremos apuntar.
 
     // Privados
-    private MyTPCharacter tpc;          // Script asociado al modelo del robot. Uso esto solo para poder animarlo
+    Animator animator;
+    //private MyTPCharacter tpc;          // Script asociado al modelo del robot. Uso esto solo para poder animarlo
     private Transform aimTarget;        // Objeto que situaremos en el centro de la pantalla al apuntar, y hacia el que mirará el personaje.
     private int aimDistance = 5;        // Distancia a la que situamos 'aimTarget' frente a nuestro personaje.
     private CinemachineFreeLook cfl;    // Este es el objeto que se crea en la escena al crear una cámara Cinemachine. Lo usamos para controlar la posición y movimiento de la cámara
@@ -29,7 +30,8 @@ public class RE_ThirdPersonInput : ThirdPersonUserControl
     private void Awake()
     {
         cfl = FindObjectOfType<CinemachineFreeLook>();
-        tpc = FindObjectOfType<MyTPCharacter>();
+        //tpc = FindObjectOfType<MyTPCharacter>();
+        animator = GetComponent<Animator>();
         aimTarget = GameObject.FindGameObjectWithTag("AimTarget").transform;
         canvas = FindObjectOfType<Canvas>();
         aimCursor = canvas.transform.Find("AimCursor").gameObject.GetComponent<Image>();
@@ -45,7 +47,7 @@ public class RE_ThirdPersonInput : ThirdPersonUserControl
         bool aiming = Input.GetKey(KeyCode.Q);              // true mientras el botón de apuntar esté pulsado
         
         
-        tpc.GetAnimator().SetBool("IsWalking", walking);    // Se le dice al Animator que el personaje está andando    
+        //tpc.GetAnimator().SetBool("IsWalking", walking);    // Se le dice al Animator que el personaje está andando    
 
         // Se coloca el 'aimTarget' en el centro de la cámara. Nuestro personaje mirará hacia él para apuntar
         aimTarget.position = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width / 2, Screen.height / 2, aimDistance));
@@ -61,9 +63,15 @@ public class RE_ThirdPersonInput : ThirdPersonUserControl
             m_Move = new Vector3(0, 0, 0);  
 
             // Rotamos al personaje para que apunte al 'aimTarget'. En concreto lo que apunta a 'aimTarget' es el pecho (chest) del personaje.
-            Vector3 aimVector = aimTarget.position - chest.position;
+            float x = aimTarget.position.x - chest.position.x;
+            float z = aimTarget.position.z - chest.position.z;
+            //Vector3 aimVector = aimTarget.position - chest.position;
+            Vector3 aimVector = new Vector3(x, 0, z);
             Quaternion rotation = Quaternion.LookRotation(aimVector, Vector3.up);
             transform.rotation = rotation;
+            Debug.Log(x);
+            Debug.Log(aimVector);
+            Debug.Log(rotation);
 
         }
         else {  // Si no está apuntando
@@ -74,7 +82,8 @@ public class RE_ThirdPersonInput : ThirdPersonUserControl
 
         // Llamamos al Move del padre para que mueva el GameObject
         m_Character.Move(m_Move, crouch, m_Jump);
-        tpc.GetAnimator().SetBool("IsAiming", aiming);
+        //tpc.GetAnimator().SetBool("IsAiming", aiming);
+        animator.SetBool("IsAiming", aiming);
         aimCursor.gameObject.SetActive(aiming);
         m_Jump = false;
     }
@@ -94,9 +103,9 @@ public class RE_ThirdPersonInput : ThirdPersonUserControl
 
                 cfl.m_Orbits[2].m_Height = 0.65f;
 
-                cfl.GetRig(0).GetCinemachineComponent<CinemachineComposer>().m_ScreenX = 0.3f;
-                cfl.GetRig(1).GetCinemachineComponent<CinemachineComposer>().m_ScreenX = 0.3f;
-                cfl.GetRig(2).GetCinemachineComponent<CinemachineComposer>().m_ScreenX = 0.3f;
+                cfl.GetRig(0).GetCinemachineComponent<CinemachineComposer>().m_ScreenX = 0.5f;
+                cfl.GetRig(1).GetCinemachineComponent<CinemachineComposer>().m_ScreenX = 0.5f;
+                cfl.GetRig(2).GetCinemachineComponent<CinemachineComposer>().m_ScreenX = 0.5f;
                 break;
             case CAMERA_MODE.AIM:   // Esta será la configuración de la cámara en el modo de apuntado
                 // Top Ring
