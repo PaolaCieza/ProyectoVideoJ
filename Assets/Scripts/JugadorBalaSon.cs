e3ac636a4577a5d557ab37d62716f6df6caaca84
PaolaCieza <paolacieza8@gmail.com>
2021-11-25 12:15:23 -0500

    piniendo sonido de disparo y particula malolgrada

---
+++ Assets/Scripts/JugadorBalaSon.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JugadorBalaSon : MonoBehaviour
+{
+     private AudioSource sonido;
+    public AudioClip sonidobala;
+    void Start()
+    {
+        sonido = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1")){
+            sonido.clip = sonidobala;
+            sonido.Play();
+        }
+    }
+}
