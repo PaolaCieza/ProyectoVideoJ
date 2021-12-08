324583d9adb9bcd63a32c18285b59f06015284cb
PaolaCieza <paolacieza8@gmail.com>
2021-12-08 13:14:48 -0500

    animaciones en la intro

--- Assets/Scripts/Intro.cs
+++ Assets/Scripts/Intro.cs
@@ -9,6 +9,7 @@ public class Intro : MonoBehaviour
     public float tiempo_end; 
     private AudioSource sonido;
     public AudioClip llamada;
+    public Animator animacion;  
 
     void Start()
     {
@@ -20,7 +21,7 @@ public class Intro : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.P)){
             sonido.clip = llamada;
             sonido.Play();
-            
+            animacion.SetTrigger("Neutro");
         }
 
         tiempo_start += Time.deltaTime;
